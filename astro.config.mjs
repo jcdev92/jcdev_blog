9749d24528d05a5680eb218a483eaca29829b913
jcdev <jcdeveloper92@gmail.com>
2026-01-13 21:03:24 -0400

    Enables Keystatic integration
    Adds Keystatic CMS, React, Markdoc integrations, and Vercel adapter for improved content management and deployment.
    
    Configures Astro to utilize these integrations, enhancing the project's capabilities.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
@@ -10,8 +16,11 @@ export default defineConfig({
   },
 
   site: "https://elblogdelgzus.netlify.app/",
-  integrations: [],
+  integrations: [react(), markdoc(), keystatic()],
+
   vite: {
     plugins: [/** @type {any} */ (tailwindcss())],
   },
+
+  adapter: vercel(),
 });
